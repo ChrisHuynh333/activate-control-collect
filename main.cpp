6f38821b89582202900e2ec54c68fef0fc4201c7
Chris Huynh <chris.huynh333@gmail.com>
2023-03-21 21:57:37 -0500

    Add movement of squares and generation of new squares during ongoing game

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "raylib.h"
 #include <vector>
+#include <chrono>
 
 class Player 
 {
@@ -105,7 +106,7 @@ void create_starting_game_squares(std::vector<Game_Square> &game_squares, std::s
     set_game_square_colors(game_squares, player, colors, game_square_type);
     
 
-    int starting_y_coord = 520 - (starting_position * 180);
+    int starting_y_coord = 500 - (starting_position * 180);
     for(int i = 0; i < game_squares.size(); i++)
     {
         game_squares[i].set_y_coord(starting_y_coord);
@@ -140,29 +141,74 @@ void initialize_game(const std::vector<Color> &available_colors, std::vector<std
     }
 }
 
+void move_game_squares(std::vector<std::vector<Game_Square>> &game_squares, const std::vector<Color> &available_colors, Player &player, std::string &color_tracker)
+{
+    for(int i = 0; i < game_squares.size(); i++)
+    {
+        for(int j = 0; j < game_squares[i].size(); j++)
+        {
+            if (game_squares[i][j].height == 30)
+            {
+                game_squares[i][j].height = 60;
+            }
+            game_squares[i][j].set_y_coord(game_squares[i][j].y + 3);
 
+            
+        }
+    }
+    if (game_squares[0][1].y >= 800)
+    {
+        game_squares.erase(game_squares.begin());
+    }
 
+    if (game_squares.back()[1].y >= 260)
+    {
+        Game_Square new_game_square_one;
+        Game_Square new_game_square_two;
+        std::vector<Game_Square> new_game_squares = {new_game_square_one, new_game_square_two};
+        set_game_square_x_coords(new_game_squares);
+        set_game_square_colors(new_game_squares, player, available_colors, color_tracker);
+        if (color_tracker == "red")
+        {
+            color_tracker = "not red";
+        }
+        else
+        {
+            color_tracker = "red";
+        }
+        for (int i = 0; i < new_game_squares.size(); i++)
+        {
+            new_game_squares[i].height = 30;
+            new_game_squares[i].set_y_coord(80);
+        }
+        game_squares.push_back(new_game_squares);
+    }
+}
 
 int main()
 {
     srand (time(NULL));
-    const std::vector<Color> colors {RED, WHITE, BLUE, GREEN, PURPLE, YELLOW, ORANGE};
+    const std::vector<Color> colors {RED, WHITE, BLUE, GREEN, PURPLE, ORANGE};
     std::vector<std::vector<Game_Square>> game_squares;
     Player player;
+    auto start_time = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> time_difference;
+    double time_tracker = 0.5;
+    std::string color_tracker = "red";
     int windowHeight = 800;
     int windowWidth = 600;
 
     InitWindow(windowWidth, windowHeight, "Collect");
     initialize_game(colors, game_squares, player);
-    SetTargetFPS(20);
+    SetTargetFPS(240);
     while(!WindowShouldClose())
     {
         BeginDrawing();
         ClearBackground(WHITE);
         DrawRectangle(player.x, player.y, player.width, player.height, player.color);
-        DrawLine(210, 100, 210, 800, BLACK);
-        DrawLine(390, 100, 390, 800, BLACK);
-        DrawLine(210, 100, 390, 100, BLACK);
+        DrawLine(210, 80, 210, 800, BLACK);
+        DrawLine(390, 80, 390, 800, BLACK);
+        DrawLine(210, 80, 390, 80, BLACK);
         for(int i = 0; i < game_squares.size(); i++)
         {
             for(int j = 0; j < game_squares[i].size(); j++)
@@ -170,27 +216,44 @@ int main()
                 DrawRectangle(game_squares[i][j].x, game_squares[i][j].y, game_squares[i][j].width, game_squares[i][j].height, game_squares[i][j].color);
             }
         }
+
+        auto end = std::chrono::high_resolution_clock::now();
+        time_difference = end - start_time;
+        if (time_difference.count() > time_tracker)
+        {
+            time_tracker += 0.01;
+            std::cout << time_tracker << "\n";
+            move_game_squares(game_squares, colors, player, color_tracker);
+            // if (seconds == 0) 
+            // {
+            //     if (minutes > 0)
+            //     {
+            //         seconds = 59;
+            //         minutes -= 1;
+            //     }
+            //     else
+            //     {
+            //         game_over = true;
+            //         DrawText("Game Over! Out Of Time", 320, 310, 50, RED);
+            //     }
+            // }
+            // else
+            // {
+            //     seconds -= 1;
+            // }
+        }
+
         if(IsKeyDown(KEY_LEFT))
         {
-            if (player.x == 270)
-            {
-                player.x = 210;
-            }
-            else if (player.x == 330)
-            {
-                player.x = 270;
-            }
+            player.x = 210;
         }
-        if(IsKeyDown(KEY_RIGHT))
+        else if (IsKeyDown(KEY_RIGHT))
         {
-            if (player.x == 210)
-            {
-                player.x = 270;
-            }
-            else if (player.x == 270)
-            {
-                player.x = 330;
-            }
+            player.x = 330;
+        }
+        else
+        {
+            player.x = 270;
         }
         EndDrawing();
     }
